d29211805d55cabd302b3839b2d3610b39a7d354
AsiaZmudziak <asiazmudziak@gmail.com>
2020-11-02 20:56:12 +0000

    change init: function

--- src/js/script.js
+++ src/js/script.js
@@ -137,18 +137,15 @@
 
     init: function(){
       const thisApp = this;
-      console.log('*** App starting ***');
+      console.log('*App starting*');
       console.log('thisApp:', thisApp);
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
-    },
-
       thisApp.initData();
-
       thisApp.initMenu();
     }
-  };
+    };
 
   app.init();
 }
