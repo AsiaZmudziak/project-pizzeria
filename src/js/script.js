8303b3db0c619e1b47c88466b49fde54b32a293a
AsiaZmudziak <asiazmudziak@gmail.com>
2020-11-03 20:22:10 +0000

    Listening for an event

--- src/js/script.js
+++ src/js/script.js
@@ -198,6 +198,9 @@
       }
       /* END LOOP: for each paramId in thisProduct.data.params */
 
+       /* multiply price by amount */
+       price *= thisProduct.amountWidget.value;
+
       /* set the contents of thisProduct.priceElem to be the value of variable price */
       thisProduct.priceElem.innerHTML = price;
     }
@@ -206,6 +209,10 @@
       const thisProduct = this;
 
       thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
+
+      thisProduct.amountWidgetElem.addEventListener('updated', function() {
+        thisProduct.processOrder();
+      });
     }
   }
 
@@ -238,6 +245,7 @@
       /* TODO: Add validation */
 
       thisWidget.value = newValue;
+      thisWidget.announce();
       thisWidget.input.value = thisWidget.value;
     }
 
