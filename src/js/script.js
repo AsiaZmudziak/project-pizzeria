eca564b81e92644f75a80b83264f235daf675856
AsiaZmudziak <asiazmudziak@gmail.com>
2020-11-03 09:22:05 +0000

    Add event listeners for the form

--- src/js/script.js
+++ src/js/script.js
@@ -130,6 +130,21 @@
     initOrderForm() {
       const thisProduct = this;
       console.log('initOrderForm:', thisProduct);
+      thisProduct.form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for (let input of thisProduct.formInputs) {
+        input.addEventListener('change', function() {
+          thisProduct.processOrder();
+        });
+      }
+
+      thisProduct.cartButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
     }
 
     processOrder() {
