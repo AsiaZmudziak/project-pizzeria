32e27b9e90f11294e0a744aa6d476ed7f55405a8
AsiaZmudziak <asiazmudziak@gmail.com>
2020-11-03 18:22:00 +0000

    finding widget elements

--- src/js/script.js
+++ src/js/script.js
@@ -201,7 +201,7 @@
       /* set the contents of thisProduct.priceElem to be the value of variable price */
       thisProduct.priceElem.innerHTML = price;
     }
-    
+
     initAmountWidget() {
       const thisProduct = this;
 
@@ -213,8 +213,19 @@
     constructor(element) {
       const thisWidget = this;
 
+      thisWidget.getElements(element);
+
       console.log('AmountWidget:', thisWidget);
       console.log('constructor arguments:', element);
+    }
+    
+    getElements(element) {
+      const thisWidget = this;
+
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
     }
   }
 
