cec791f111e5574a4b0a2680812ef740c8a371b6
unknown <asiazmudziak@gmail.com>
2020-11-06 15:13:25 +0100

    sending the product to the basket

--- src/js/script.js
+++ src/js/script.js
@@ -167,6 +167,7 @@
       thisProduct.cartButton.addEventListener('click', function(event) {
         event.preventDefault();
         thisProduct.processOrder();
+        thisProduct.addToCart();
       });
     }
 
@@ -241,6 +242,12 @@
         thisProduct.processOrder();
       });
     }
+    addToCart() {
+      const thisProduct = this;
+
+      app.cart.add(thisProduct);
+    }
+
   }
 
   class AmountWidget {
@@ -333,6 +340,11 @@
       thisCart.dom.toggleTrigger.addEventListener('click', function() {
         thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
       });
+    }
+    add(menuProduct) {
+      //const thisCart = this;
+
+      console.log('adding product', menuProduct);
     }
   }
 
