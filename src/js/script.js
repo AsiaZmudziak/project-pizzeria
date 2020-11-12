14844aa0754892329b1fc5478ba984719844c51c
unknown <asiazmudziak@gmail.com>
2020-11-12 23:52:09 +0100

    removing product from basket

--- src/js/script.js
+++ src/js/script.js
@@ -433,9 +433,9 @@
 
       thisCartProduct.getElements(element);
       thisCartProduct.initAmountWidget();
+      thisCartProduct.initActions();
 
-      console.log('new CartProduct', thisCartProduct);
-      console.log('productData', menuProduct);
+      
     }
 
     getElements(element) {
@@ -458,6 +458,32 @@
         thisCartProduct.price = thisCartProduct.priceSingle * thisCartProduct.amount;
         console.log(thisCartProduct.price);
         thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
+      });
+    }
+
+    remove() {
+      const thisCartProduct = this;
+
+      const event = new CustomEvent('remove', {
+        bubbles: true,
+        detail: {
+          cartProduct: thisCartProduct
+        }
+      });
+
+      thisCartProduct.dom.wrapper.dispatchEvent(event);
+    }
+
+    initActions() {
+      const thisCartProduct = this;
+
+      thisCartProduct.dom.edit.addEventListener('click', function(event) {
+        event.preventDefault();
+      });
+
+      thisCartProduct.dom.remove.addEventListener('click', function(event) {
+        event.preventDefault();
+        thisCartProduct.remove();
       });
     }
   }
