f94f3b33992621c3141ad4bf9804099daabc809b
unknown <asiazmudziak@gmail.com>
2020-11-12 21:10:10 +0100

    add instances to CartProduct

--- src/js/script.js
+++ src/js/script.js
@@ -373,7 +373,7 @@
       
       //console.log('adding product', menuProduct);
 
-      thisCart.products.push(menuProduct);
+      thisCart.products.push(new CartProduct(menuProduct, generatedDOM));
       console.log('thisCart.products', thisCart.products);
     }
   }
