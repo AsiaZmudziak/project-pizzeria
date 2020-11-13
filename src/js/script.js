cd30b1178843cc1ce07cde6f2a19346d7206fcf6
unknown <asiazmudziak@gmail.com>
2020-11-13 19:43:48 +0100

    sending order to API

--- src/js/script.js
+++ src/js/script.js
@@ -365,6 +365,8 @@
       for (let key of thisCart.renderTotalsKeys) {
         thisCart.dom[key] = thisCart.dom.wrapper.querySelectorAll(select.cart[key]);
       }
+
+      thisCart.dom.form = thisCart.dom.wrapper.querySelector(select.cart.form);
     }
 
     initActions() {
@@ -381,9 +383,14 @@
       thisCart.dom.productList.addEventListener('remove', function() {
         thisCart.remove(event.detail.cartProduct);
       });
+
+      thisCart.dom.form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        thisCart.sendOrder();
+      });
     }
 
-
+    
     add(menuProduct) {
       const thisCart = this;
       //console.log('adding product', menuProduct);
@@ -442,6 +449,26 @@
       thisCart.update();
     }
 
+    sendOrder(){
+      const thisCart = this;
+      const url = settings.db.url + '/' + settings.db.order;
+
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      };
+
+      fetch(url, options)
+        .then(function(response){
+          return response.json();
+        })
+        .then(function(parsedResponse){
+          console.log('parsedResponse', parsedResponse);
+        });
+    }
   }
 
   class CartProduct {
