28c013758c4e8d50863691faca53918adc974acd
unknown <asiazmudziak@gmail.com>
2020-11-19 00:44:00 +0100

    DatePicker method and new plugin part2

--- src/js/components/DatePicker.js
+++ src/js/components/DatePicker.js
@@ -1,3 +1,31 @@
 class DatePicker extends BaseWidget {
+    super(wrapper, utils.dateToStr(new Date()));
+    const thisWidget = this;
     
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.datePicker.input);
+
+    thisWidget.initPlugin();
+}
+
+initPlugin() {
+    const thisWidget = this;
+    
+    /* create thisWidget.minDate and equal to new Date (thisWidget.value) */
+    thisWidget.minDate = new Date(thisWidget.value);
+
+    /* is to create thisWidget.maxDate date after thisWidget.minDate by the amount in settings.datePicker.maxDaysInFuture */
+    thisWidget.maxDate = utils.addDays(thisWidget.minDate, settings.datePicker.maxDaysInFuture);
+
+}
+
+parseValue(newValue){
+    return newValue;
+  }
+
+  isValid(){
+    event.preventDefault();
+    return true;
+  }
+  
+  renderValue(){}
 }
