b198dd8a0136ea0beb5bb12c47bcd392a4a89c32
unknown <asiazmudziak@gmail.com>
2020-11-19 04:59:55 +0100

    HourPicker part2

--- src/js/components/DatePicker.js
+++ src/js/components/DatePicker.js
@@ -3,7 +3,9 @@ import {utils} from '../utils.js';
 import {select, settings} from '../settings.js';
 
 class DatePicker extends BaseWidget {
+    constructor(wrapper) {
     super(wrapper, utils.dateToStr(new Date()));
+    
     const thisWidget = this;
     
     thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.datePicker.input);
