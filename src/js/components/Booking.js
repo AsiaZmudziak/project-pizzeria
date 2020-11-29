aa1e96b839679e26e60c2ae56e64fdfdf4852363
unknown <asiazmudziak@gmail.com>
2020-11-29 21:21:20 +0100

    Sending reservations to API

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -10,6 +10,68 @@ export class Booking {
     thisBooking.render(bookingWidget);
     thisBooking.initWidgets();
     thisBooking.getData();
+    thisBooking.initActions();
+  }
+
+  initActions(){
+    const thisBooking = this;
+    const tables = thisBooking.dom.tables;
+    let bookedTable = '';
+
+    /* pick one of avaiable table */
+    for (let table of tables){
+      table.addEventListener('click', function(){
+        table.classList.add('booked');
+        bookedTable = table.getAttribute('data-table');
+        thisBooking.table = bookedTable;
+      });
+    }
+
+    /* remove picked table while date or hour change */
+    thisBooking.hourPicker.dom.input.addEventListener('input', function(){
+      if (bookedTable.length > 0) {
+        tables[bookedTable-1].classList.remove('booked');
+      }
+    });
+
+    thisBooking.datePicker.dom.input.addEventListener('input', function(){
+      if (bookedTable.length > 0) {
+        tables[bookedTable-1].classList.remove(booked);
+      }
+    });
+    thisBooking.dom.bookButton.addEventListener('submit', function(){
+      event.preventDefault();
+      thisBooking.sendBooking();
+    });
+  }
+
+  sendBooking(){
+    const thisBooking = this;
+    const url = settings.db.url + '/' + settings.db.booking;
+
+    const payload = {
+      date: thisBooking.datePicker.correctValue,
+      hour: thisBooking.hourPicker.correctValue,
+      duration: thisBooking.hoursAmount.correctValue,
+      ppl: thisBooking.peopleAmount.correctValue,
+      table: thisBooking.parseInt(thisBooking.table),
+      repeat: false,
+      id: '',
+    };
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    };
+    fetch(url, options)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse: ', parsedResponse);
+    });
   }
 
   getData() {
